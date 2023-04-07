d478f4594809634e7c1c7ed778f898bbcc5bc231
FiveWing96 <117134067+FiveWing96@users.noreply.github.com>
2023-04-07 11:00:48 +0700

    Second_Data

--- src/binarySearch/Main.java
+++ src/binarySearch/Main.java
@@ -1,2 +1,15 @@
-package binarySearch;public class Main {
+package binarySearch;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+        System.out.println(Test.binarySearch(list,2));  /* 0 */
+        System.out.println(Test.binarySearch(list, 11)); /* 4 */
+        System.out.println(Test.binarySearch(list, 79)); /*12 */
+        System.out.println(Test.binarySearch(list, 1));  /*-1 */
+        System.out.println(Test.binarySearch(list, 5));  /*-1 */
+        System.out.println(Test.binarySearch(list, 80)); /*-1 */
+    }
+
 }
